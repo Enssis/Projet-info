d10b0bf73ea35d0e923b1fb6d68f10fa4406c345
HéloïseDuhen <33767@192.168.1.20>
2021-04-22 17:03:49 +0200

    rerebonzoir

--- src/main/java/fr/insa/a6/treillis/noeuds/Noeud.java
+++ src/main/java/fr/insa/a6/treillis/noeuds/Noeud.java
@@ -3,23 +3,37 @@ package fr.insa.a6.treillis.noeuds;
 import fr.insa.a6.treillis.Barres;
 import fr.insa.a6.treillis.Force;
 import fr.insa.a6.treillis.dessin.Point;
+import java.util.ArrayList;
 
 /**
  * 
  */
+
 public abstract class Noeud extends Point {
 
     private Force forceApplique;
-    private Barres[] linkedBarres;
+    private ArrayList<Barres> linkedBarres;
 
     /**
      * Default constructor
      */
-    public Noeud(int id, Force forceApplique, Barres[] linkedBarres) {
+    public Noeud(int id, Force forceApplique, ArrayList<Barres> linkedBarres) {
         
     this.id = id;
     this.forceApplique = forceApplique;
     this.linkedBarres= linkedBarres;
     
+    }
+    
+    public void addBarres(Barres barre){
+        linkedBarres.add(barre);
+    }
+    
+    public void removeBarres (Barres barre){
+        linkedBarres.remove(barre);
+    }
+    
+    public Noeud (){
+        //rien du tout
     }
 }
