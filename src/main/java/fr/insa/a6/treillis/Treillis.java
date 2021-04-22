3b8e368efa9edf5207515f48bdd400cdbd0a44e5
HéloïseDuhen <33767@192.168.1.20>
2021-04-22 17:02:54 +0200

    rebonzoir

--- src/main/java/fr/insa/a6/treillis/Treillis.java
+++ src/main/java/fr/insa/a6/treillis/Treillis.java
@@ -2,15 +2,17 @@ package fr.insa.a6.treillis;
 
 import fr.insa.a6.treillis.noeuds.Noeud;
 import fr.insa.a6.treillis.terrain.Terrain;
+import java.util.ArrayList;
+
 
 /**
  * 
  */
 public class Treillis {
 
-    private Terrain[] terrains;
-    private Noeud[] noeud;
-    private Barres[] barres;
+    private ArrayList<Terrain> terrains;
+    private ArrayList <Noeud> noeuds;
+    private ArrayList<Barres> barres;
     private Type[] catalogueType;
 
     /**
@@ -19,6 +21,20 @@ public class Treillis {
     public Treillis() {
     }
 
-
+    
+    
+ public void addBarres(Barres barre){
+        barres.add(barre);    }
+    
+    public void removeBarres (Barres barre){
+        barres.remove(barre);    }
+    
+     public void addNoeuds(Noeud noeud){
+        noeuds.add(noeud); }
+    
+    public void removeNoeuds (Noeud noeud){
+        noeuds.remove(noeud); }
+    
+    
 
 }
